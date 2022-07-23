0a13ab56054f7beb9f86b81c88baf23941a8bd9a
larrieking <larrie4christ@gmail.com>
2022-07-23 17:36:46 +0100

    Update config.py
    login url masked

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from flask_pymongo import PyMongo
 def configurations():
     app = Flask(__name__)
     app.config[
-        "MONGO_URI"] = "mongodb+srv://larryking:Elshadai1986@cluster0.vo53l.mongodb.net/user?retryWrites=true&w=majority"
+        "MONGO_URI"] = "<manoddb_atlas login url>"
     app.secret_key = 'Allah198612345678'
     return app
 
